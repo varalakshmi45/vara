628c0f4b70869957736f6135581626166ec1b0da
VaralakshmiPerumalla <indhuvaralakshmi45@gmail.com>
2026-02-13 01:26:32 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ let count = 0;
 const countDisplay = document.getElementById("count");
 const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
+const updateBtn = document.getElementById("update");
+const container = document.querySelector(".container");
+
+function checkMaxReached() {
+  if (count >= 10) {
+    container.classList.add("fade-out");
+    increaseBtn.disabled = true;
+    decreaseBtn.disabled = true;
+    updateBtn.style.display = "inline-block";
+  } else {
+    container.classList.remove("fade-out");
+    increaseBtn.disabled = false;
+    decreaseBtn.disabled = false;
+    updateBtn.style.display = "none";
+  }
+}
 
 increaseBtn.addEventListener("click", function () {
   if (count < 10) {
     count++;
     countDisplay.innerText = count;
+    checkMaxReached();
   }
 });
 
@@ -15,5 +32,12 @@ decreaseBtn.addEventListener("click", function () {
   if (count > 0) {
     count--;
     countDisplay.innerText = count;
+    checkMaxReached();
   }
+});
+
+updateBtn.addEventListener("click", function () {
+  count = 0;
+  countDisplay.innerText = count;
+  checkMaxReached();
 });
